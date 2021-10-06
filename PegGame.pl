052219877f98d0349be1733378dc576966b49bdf
austinschmidt <austin.schmidt@selu.edu>
2021-10-06 09:40:03 -0500

    Generate triangles of size N

--- PegGame.pl
+++ PegGame.pl
@@ -1,132 +1,121 @@
 %A full 15 peg Crazy Triangle representation.
-:- dynamic in/2.
+:- dynamic in/2, adjacent/2,
+    l_adjacent/2,  ld_adjacent/2,  r_adjacent/2, rd_adjacent/2.
 
-%Bottom - Layer 0.
-in(peg0, hole00).
-in(peg1,hole01).
-in(peg2,hole02).
-in(peg3,hole03).
-in(peg4,hole04).
+%Deletes all associations between pegs and holes.
+%Creates new atoms for peg and hole.
+%Deletes all associations in the adjacency matrix.
+%Creates new atoms needed for adjacency matrix.
+%N = Max number of rows.
 
-%Layer 1.
-in(peg5, hole10).
-in(peg6,hole11).
-in(peg7,hole12).
-in(peg8,hole13).
+refresh_game(N):-
+    %Subtract to index at 0.
+    N2 is N-1,
 
-%Layer 2.
-in(peg9, hole20).
-in(peg10,hole21).
-in(peg11,hole22).
+    %Retracting predicates.
+    retractall(in(_,_)),
+    retractall(adjacent(_,_)),
+    retractall(l_adjacent(_,_)),
+    retractall(ld_adjacent(_,_)),
+    retractall(r_adjacent(_,_)),
+    retractall(rd_adjacent(_,_)),
 
-%Layer 3.
-in(peg12,hole30).
-in(peg13,hole31).
+    %Define empty hole.
+    atom_concat(hole,N2,Hole),
+    atom_concat(Hole,0,Hole2),
+    assert(in(empty,Hole2)),
 
-%Top - Layer 4.
-in(empty,hole40).
+    %Special case facts
+    H is N2 - 1,
+    atom_concat(hole,H,Hole3),
+    atom_concat(Hole3,0,Hole4),
+    assert(ld_adjacent(Hole2,Hole4)),
+    assert(r_adjacent(Hole4,Hole2)),
+    atom_concat(Hole3,1,Hole5),
+    assert(rd_adjacent(Hole2,Hole5)),
 
-%Row-wise adjacency.
-%Layer 0.
-adjacent(hole00, hole01).
-adjacent(hole01, hole00).
-adjacent(hole01, hole02).
-adjacent(hole02, hole01).
-adjacent(hole02, hole03).
-adjacent(hole03, hole02).
-adjacent(hole03, hole04).
-adjacent(hole04, hole03).
+    %Adjacency matrix and peg generation.
+    N3 is N2-1,
+    between(0,N3,X), %TODO: Combine between clauses with forall
+    Q is N2-X,
+    between(0,Q,Y),
+    set_in(X,Y),
+    set_adjacent(X,Y),
+    set_l_adjacent(X,Y),
+    set_ld_adjacent(X,Y),
+    set_r_adjacent(X,Y),
+    set_rd_adjacent(X,Y).
 
-%Layer 1.
-adjacent(hole10, hole11).
-adjacent(hole11, hole10).
-adjacent(hole11, hole12).
-adjacent(hole12, hole11).
-adjacent(hole12, hole13).
-adjacent(hole13, hole12).
+%Initializing all in(Peg,Hole).
+set_in(N,M):-
+    atom_concat(peg,N,Peg),
+    atom_concat(hole,N,Hole),
+    atom_concat(Peg,M,Peg2),
+    atom_concat(Hole,M,Hole2),
+    assert(in(Peg2,Hole2)).
 
-%Layer 2.
-adjacent(hole20, hole21).
-adjacent(hole21, hole20).
-adjacent(hole21, hole22).
-adjacent(hole22, hole21).
+%Initialize horizontal adjacency.
+set_adjacent(N,M):-
+    (atom_concat(hole,N,Hole1),
+    atom_concat(hole,N,Hole2),
+    atom_concat(Hole1,M,Hole1_2),
+    M2 is M - 1,
+    M2>=0,
+    atom_concat(Hole2,M2,Hole2_2),
+    assert(adjacent(Hole1_2,Hole2_2)),
+    assert(adjacent(Hole2_2,Hole1_2)));true.
 
-%Layer 3.
-adjacent(hole30, hole31).
-adjacent(hole31, hole30).
+%Initialize left horizontal adjacency.
+set_l_adjacent(N,M):-
+    (atom_concat(hole,N,Hole1),
+    atom_concat(Hole1,M,Hole1_2),
+    N2 is N + 1,
+    M2 is M - 1,
+    M2 >= 0,
+    atom_concat(hole,N2,Hole2),
+    atom_concat(Hole2,M2,Hole2_2),
+    \+ l_adjacent(Hole1_2, Hole2_2),
+    assert(l_adjacent(Hole1_2,Hole2_2)));true.
 
-%Column-wise adjacency.
-% Since pegs live between pegs on higher layers, we can jump vertically
-% left or right. LCol = (Col, Row-1) and RCol = (Col+1, Row-1).
-% Must jump from left adjacent to the right adjacent or the right
-% adjacent to left adjacent.
+%Initialize right horizontal adjacency
+set_r_adjacent(N,M):-
+    (atom_concat(hole,N,Hole1),
+    atom_concat(Hole1,M,Hole1_2),
+    N2 is N - 1,
+    N2 >= 0,
+    atom_concat(hole,N2,Hole2),
+    atom_concat(Hole2,M,Hole2_2),
+    \+ r_adjacent(Hole2_2, Hole1_2),
+    assert(r_adjacent(Hole2_2,Hole1_2)));true.
 
-%Left adjacency
-l_adjacent(hole01, hole10).
-l_adjacent(hole02, hole11).
-l_adjacent(hole03, hole12).
-l_adjacent(hole04, hole13).
+%Initialize left downward horizontal adjacency.
+set_ld_adjacent(N,M):-
+    (atom_concat(hole,N,Hole1),
+    atom_concat(Hole1,M,Hole1_2),
+    N2 is N - 1,
+    N2 >= 0,
+    atom_concat(hole,N2,Hole2),
+    atom_concat(Hole2,M,Hole2_2),
+    \+ ld_adjacent(Hole1_2, Hole2_2),
+    assert(ld_adjacent(Hole1_2,Hole2_2)));true.
 
-l_adjacent(hole11, hole20).
-l_adjacent(hole12, hole21).
-l_adjacent(hole13, hole22).
+%Initialize right downward horizontal adjacency.
+set_rd_adjacent(N,M):-
+    (atom_concat(hole,N,Hole1),
+    atom_concat(Hole1,M,Hole1_2),
+    N2 is N - 1,
+    M2 is M + 1,
+    N2 >= 0,
+    atom_concat(hole,N2,Hole2),
+    atom_concat(Hole2,M2,Hole2_2),
+    \+ rd_adjacent(Hole1_2, Hole2_2),
+    assert(rd_adjacent(Hole1_2,Hole2_2)));true.
 
-l_adjacent(hole21, hole30).
-l_adjacent(hole22, hole31).
-
-l_adjacent(hole31, hole40).
-
-%test
-ld_adjacent(hole10, hole00).
-ld_adjacent(hole11, hole01).
-ld_adjacent(hole12, hole02).
-ld_adjacent(hole13, hole03).
-
-ld_adjacent(hole20, hole10).
-ld_adjacent(hole21, hole11).
-ld_adjacent(hole22, hole12).
-
-ld_adjacent(hole30, hole20).
-ld_adjacent(hole31, hole21).
-
-ld_adjacent(hole40, hole30).
-
-%Right adjacency
-r_adjacent(hole00, hole10).
-r_adjacent(hole01, hole11).
-r_adjacent(hole02, hole12).
-r_adjacent(hole03, hole13).
-
-r_adjacent(hole10, hole20).
-r_adjacent(hole11, hole21).
-r_adjacent(hole12, hole22).
-
-r_adjacent(hole20, hole30).
-r_adjacent(hole21, hole31).
-
-r_adjacent(hole30, hole40).
-
-%test
-rd_adjacent(hole10, hole01).
-rd_adjacent(hole11, hole02).
-rd_adjacent(hole12, hole03).
-rd_adjacent(hole13, hole04).
-
-rd_adjacent(hole20, hole11).
-rd_adjacent(hole21, hole12).
-rd_adjacent(hole22, hole13).
-
-rd_adjacent(hole30, hole20).
-rd_adjacent(hole31, hole22).
-
-rd_adjacent(hole40, hole31).
-
-
-
+%Checks for jumps in all directions.
 can_jump(P1, P2, H) :-
     can_jump_h(P1,P2,H);
-    can_jump_vl(P1,P2,H);
-    can_jump_vr(P1,P2,H).
+    can_jump_vr(P1,P2,H);
+    can_jump_vl(P1,P2,H).
 
 can_jump_vl(P1, P2, H):-
     P1 \== P2,
@@ -167,7 +156,7 @@ can_jump_h(P1, P2, H):-
     adjacent(X,Y),
     adjacent(Y,H).
 
-
+%Notify of jump and update facts.
 jump(P1,P2,H) :-
     can_jump(P1,P2,H),
     out(P1,P2,H),
@@ -188,11 +177,19 @@ out(P1,P2,H) :-
     write(' into '),
     write(H), write('\n').
 
+%Plays game until one peg remains, or when no jumps remain.
 play(1):-write('Only one left! Win!'),nl.
 play(N):-
     N>1,
     jump(P1,P2,H),
     S is N-1,
     play(S).
+
+
+
+
+
+
+
 
 
