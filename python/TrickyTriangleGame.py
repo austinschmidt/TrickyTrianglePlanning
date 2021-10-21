49bc75cc5dc089dede68d5a910cd9d811e8d766a
Anthony Marchiafava <amarchia@uno.edu>
2021-10-21 10:49:53 -0500

    adding more methods

--- python/TrickyTriangleGame.py
+++ python/TrickyTriangleGame.py
@@ -14,7 +14,41 @@ class Hole():
     def add_line(self, line):
         self.lines.append(line)
 
+    def insert_peg(self, peg):
+        if self.peg is None:
+            self.peg = peg
+            return True
+        else: 
+            return False
+
 class Board:
+    def peg_in(self, peg, hole):
+        if self.holes[hole.peg.id].id is peg.id:
+            return True
+        else:
+            return False
+
+    def peg_exists(self, peg):
+        for h in self.holes:
+            if h.peg.id is peg.id:
+                return True
+        return False
+
+    def hole_empty(self, hole):
+        if self.holes[hole.id].peg is None:
+            return True
+        else:
+            return False
+
+    def check_if_move_allowed(self, peg, source_hole, over_hole, destination_hole):
+        if peg_exists(peg) and peg_in(peg, source_hole) and not hole_empty(over_hole) and hole_empty(destination_hole):
+           return True
+        else:
+            return False
+
+    def remove_peg(self, hole):
+        self.holes[hole.id].peg = None
+
     def __init__(self, num_levels):
         self.holes = []
         self.levels = []
@@ -111,5 +145,23 @@ class Board:
         self.lines.append([self.holes[9],self.holes[5], self.holes[2]])
         self.lines.append([self.holes[9],self.holes[8], self.holes[7]])
 
+        count = 0
+        for location in self.holes:
+            location.insert_peg(Peg(count))
+            count = count +1
+
+        #remove the peg from hole 2
+        self.remove_peg(self.holes[2])
+
 if __name__ == "__main__":
     game = Board(4)
+    #should be true
+    print(game.peg_in(game.holes[0].peg, game.holes[0]))
+    #should be true
+    print(game.peg_exists(game.holes[0].peg))
+    #should be false
+    print(game.hole_empty(game.holes[0]))
+
+    #shoudl be true
+    print(game.hole_empty(game.holes[2]))
+    print()
