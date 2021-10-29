90e4c5a1c92f0c7b4f792cda4528e6b9716c1b34
Anthony Marchiafava <amarchia@uno.edu>
2021-10-29 15:04:56 -0500

    added bfs

--- python/TrickyTriangleGame.py
+++ python/TrickyTriangleGame.py
@@ -1,9 +1,11 @@
 #partially based on https://keras.io/examples/rl/actor_critic_cartpole/
 import copy
+import collections
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
+
 
 from PythonAdjacency import set_holes, set_lines
 
@@ -176,122 +178,174 @@ class Board:
                 
         return self.get_state(), reward, done, {}
 
+class GameStateGraph:
+    class Node:
+        def __init__(self, element):
+            self.viewed = False
+            self.children = list()
+            self.element = element
+        def add_child(self, child_elememnt):
+            child = Node(child_element)
+            self.childen.append(child)
+        def get_children(self):
+            return self.children
+        def mark_as_viewed(self):
+            self.viewed = True
+        def isViewed(self):
+            return self.viewed
+        def get_element(self):
+            return self.element
+        def create_children(self):
+            children = list()
+            moves = self.element.legal_moves()
+            for i in range(len(moves)):
+                new = self.element.create_copy()
+                choice = new.legal_moves()[i]
+                a,b,c,d = choice
+                new.move_peg(a,b,c,d)
+                self.children.append(GameStateGraph.Node(new))
+
+
+    def __init__(self, first_node_element):
+        self.head = self.Node(first_node_element)
+        self.create_graph(self.head)
+    
+    def get_head(self):
+        return self.head
+
+    def create_graph(self, node):
+        node.create_children()
+        if len(node.get_children()) == 0:
+            return 
+        else:
+            for child in node.get_children():
+                return self.create_graph(child)
+
+
+
 if __name__ == "__main__":
     game = Board(4)
 
-    moves = game.legal_moves()
-    for i in range(len(moves)):
-        new = game.create_copy()
-        choice = new.legal_moves()[i]
-        a,b,c,d = choice
-        new.move_peg(a,b,c,d)
-        print()
+    bfs = True
+    if bfs:
+        game_states = GameStateGraph(game)
+        q = collections.deque()
+        s = game_states.get_head()
+        q.append(s)
+        s.mark_as_viewed()
+        while len(q)!= 0:
+            v = q.popleft()
+            for child in v.get_children():
+                if child.isViewed is False:
+                    q.append(child)
+                    child.mark_as_viewed()
     
+    machine_learning = False
+    if machine_learning:
+        #actor critic model
+        # Configuration parameters for the whole setup
+        gamma = 0.99  # Discount factor for past rewards
+        max_steps_per_episode = 100
+        num_inputs = len(game.holes)
+        num_actions = len(game.lines)
+        eps = np.finfo(np.float32).eps.item()
     
-    #actor critic model
-    # Configuration parameters for the whole setup
-    gamma = 0.99  # Discount factor for past rewards
-    max_steps_per_episode = 100
-    num_inputs = len(game.holes)
-    num_actions = len(game.lines)
-    eps = np.finfo(np.float32).eps.item()
-    
-    num_hidden = 128
+        num_hidden = 128
 
-    inputs = layers.Input(shape=(num_inputs,))
-    x = layers.Dense(num_hidden, activation="relu")(inputs)
-    common = layers.Dense(num_hidden, activation="relu")(x)
-    #common = layers.Dense(num_hidden, activation="relu")(common)
-    action = layers.Dense(num_actions, activation="softmax")(common)
-    critic = layers.Dense(1)(common)
+        inputs = layers.Input(shape=(num_inputs,))
+        x = layers.Dense(num_hidden, activation="relu")(inputs)
+        common = layers.Dense(num_hidden, activation="relu")(x)
+        #common = layers.Dense(num_hidden, activation="relu")(common)
+        action = layers.Dense(num_actions, activation="softmax")(common)
+        critic = layers.Dense(1)(common)
 
-    model = keras.Model(inputs=inputs, outputs=[action, critic])
-    model.summary()
-    optimizer = keras.optimizers.Adam(learning_rate=0.02)
-    huber_loss = keras.losses.Huber()
-    #huber_loss = keras.losses.MeanSquaredError()
-    action_probs_history = []
-    critic_value_history = []
-    rewards_history = []
-    running_reward = 0
-    episode_count = 0
+        model = keras.Model(inputs=inputs, outputs=[action, critic])
+        model.summary()
+        optimizer = keras.optimizers.Adam(learning_rate=0.02)
+        huber_loss = keras.losses.Huber()
+        #huber_loss = keras.losses.MeanSquaredError()
+        action_probs_history = []
+        critic_value_history = []
+        rewards_history = []
+        running_reward = 0
+        episode_count = 0
 
-    while True:  # Run until solved
-        state = game.reset()
-        episode_reward = 0
-        with tf.GradientTape() as tape:
-            for timestep in range(1, max_steps_per_episode):
+        while True:  # Run until solved
+            state = game.reset()
+            episode_reward = 0
+            with tf.GradientTape() as tape:
+                for timestep in range(1, max_steps_per_episode):
 
-                state = tf.convert_to_tensor(state)
-                state = tf.expand_dims(state, 0)
+                    state = tf.convert_to_tensor(state)
+                    state = tf.expand_dims(state, 0)
 
-                # Predict action probabilities and estimated future rewards
-                # from environment state
-                action_probs, critic_value = model(state)
-                critic_value_history.append(critic_value[0, 0])
+                    # Predict action probabilities and estimated future rewards
+                    # from environment state
+                    action_probs, critic_value = model(state)
+                    critic_value_history.append(critic_value[0, 0])
 
-                # Sample action from action probability distribution
-                action = np.random.choice(num_actions, p=np.squeeze(action_probs))
-                action_probs_history.append(tf.math.log(action_probs[0, action]))
+                    # Sample action from action probability distribution
+                    action = np.random.choice(num_actions, p=np.squeeze(action_probs))
+                    action_probs_history.append(tf.math.log(action_probs[0, action]))
 
-                # Apply the sampled action in our environment
-                state, reward, done, _ = game.step(action)
-                rewards_history.append(reward)
-                episode_reward += reward
+                    # Apply the sampled action in our environment
+                    state, reward, done, _ = game.step(action)
+                    rewards_history.append(reward)
+                    episode_reward += reward
 
-                if done:
-                    break
+                    if done:
+                        break
 
-            print(game.number_of_pegs)
-            # Update running reward to check condition for solving
-            running_reward = 0.05 * episode_reward + (1 - 0.05) * running_reward
+                print(game.number_of_pegs)
+                # Update running reward to check condition for solving
+                running_reward = 0.05 * episode_reward + (1 - 0.05) * running_reward
 
-            # Calculate expected value from rewards
-            # - At each timestep what was the total reward received after that timestep
-            # - Rewards in the past are discounted by multiplying them with gamma
-            # - These are the labels for our critic
-            returns = []
-            discounted_sum = 0
-            for r in rewards_history[::-1]:
-                discounted_sum = r + gamma * discounted_sum
-                returns.insert(0, discounted_sum)
+                # Calculate expected value from rewards
+                # - At each timestep what was the total reward received after that timestep
+                # - Rewards in the past are discounted by multiplying them with gamma
+                # - These are the labels for our critic
+                returns = []
+                discounted_sum = 0
+                for r in rewards_history[::-1]:
+                    discounted_sum = r + gamma * discounted_sum
+                    returns.insert(0, discounted_sum)
 
-            # Normalize
-            returns = np.array(returns)
-            returns = (returns - np.mean(returns)) / (np.std(returns) + eps)
-            returns = returns.tolist()
+                # Normalize
+                returns = np.array(returns)
+                returns = (returns - np.mean(returns)) / (np.std(returns) + eps)
+                returns = returns.tolist()
 
-            # Calculating loss values to update our network
-            history = zip(action_probs_history, critic_value_history, returns)
-            actor_losses = []
-            critic_losses = []
-            for log_prob, value, ret in history:
-                # At this point in history, the critic estimated that we would get a
-                # total reward = `value` in the future. We took an action with log probability
-                # of `log_prob` and ended up recieving a total reward = `ret`.
-                # The actor must be updated so that it predicts an action that leads to
-                # high rewards (compared to critic's estimate) with high probability.
-                diff = ret - value
-                actor_losses.append(-log_prob * diff)  # actor loss
+                # Calculating loss values to update our network
+                history = zip(action_probs_history, critic_value_history, returns)
+                actor_losses = []
+                critic_losses = []
+                for log_prob, value, ret in history:
+                    # At this point in history, the critic estimated that we would get a
+                    # total reward = `value` in the future. We took an action with log probability
+                    # of `log_prob` and ended up recieving a total reward = `ret`.
+                    # The actor must be updated so that it predicts an action that leads to
+                    # high rewards (compared to critic's estimate) with high probability.
+                    diff = ret - value
+                    actor_losses.append(-log_prob * diff)  # actor loss
 
-                # The critic must be updated so that it predicts a better estimate of
-                # the future rewards.
-                critic_losses.append(
-                    huber_loss(tf.expand_dims(value, 0), tf.expand_dims(ret, 0))
-                )
+                    # The critic must be updated so that it predicts a better estimate of
+                    # the future rewards.
+                    critic_losses.append(
+                        huber_loss(tf.expand_dims(value, 0), tf.expand_dims(ret, 0))
+                    )
 
-            # Backpropagation
-            loss_value = sum(actor_losses) + sum(critic_losses)
-            grads = tape.gradient(loss_value, model.trainable_variables)
-            optimizer.apply_gradients(zip(grads, model.trainable_variables))
+                # Backpropagation
+                loss_value = sum(actor_losses) + sum(critic_losses)
+                grads = tape.gradient(loss_value, model.trainable_variables)
+                optimizer.apply_gradients(zip(grads, model.trainable_variables))
 
-            # Clear the loss and reward history
-            action_probs_history.clear()
-            critic_value_history.clear()
-            rewards_history.clear()
+                # Clear the loss and reward history
+                action_probs_history.clear()
+                critic_value_history.clear()
+                rewards_history.clear()
 
-        # Log details
-        episode_count += 1
-        if episode_count % 10 == 0:
-            template = "running reward: {:.2f} at episode {}"
-            print(template.format(running_reward, episode_count))    
+            # Log details
+            episode_count += 1
+            if episode_count % 10 == 0:
+                template = "running reward: {:.2f} at episode {}"
+                print(template.format(running_reward, episode_count))    
